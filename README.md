f5d9c4250ae6f642f9cd4be1fca396a41eee5f25
FurryR <awathefox@gmail.com>
2023-10-14 23:29:41 +0800

    :sparkles: 极简化

--- README.md
+++ README.md
@@ -1,3 +1,88 @@
-# secure-vm
+# <p align="center">secure-vm</p>
 
-一个利用 iframe 特性实现的安全的 VM。目前支持 Edge/Firefox。
+一个利用[浏览器特性](#原理)实现的，安全、轻量、极简、透明的沙箱环境。
+
+- [secure-vm](#secure-vm)
+  - [可用性](#可用性)
+  - [使用方法](#使用方法)
+    - [导入](#导入)
+    - [基本使用](#基本使用)
+    - [向虚拟机添加外部内容](#向虚拟机添加外部内容)
+  - [原理](#原理)
+
+## 可用性
+
+secure-vm 在以下浏览器测试可用：
+
+- Edge 117.0.2045.60
+- Chromium 118.0.5993.71
+- Firefox 118.0.2
+
+## 使用方法
+
+### 导入
+
+可以将编译产物 `dist/main.js` 嵌入到任何地方，也可以使用 `ES Module` 导入。请注意包含版权信息。
+
+### 基本使用
+
+要创建一个上下文，方式如下：
+
+```js
+const ctx = vm() // 返回一个 globalThis 对象
+```
+
+然后，你可以使用 `ctx.eval` 或者 `ctx.Function` 执行代码：
+
+```js
+ctx.eval('1 + 1')
+// ctx.eval("console.log(`Hello World`)") // 因为 console.log 在 vm 中不存在，所以无效
+```
+
+整个框架就这么多内容。
+
+### 向虚拟机添加外部内容
+
+你可以通过 `ctx` 向沙盒本身添加或移除一些全局属性，secure-vm 会帮助你进行相应的隔离。
+
+甚至，即使使用 `ctx.Function = Function`，也不会导致沙盒逃逸。
+
+**警告：secure-vm 并不能保证外部内容不被滥用。如果你的函数含有 eval 或者 Function 等内容，即使 secure-vm 也无能为力。**
+
+```js
+function A() {
+  this.prototype.method = function () {
+    return new Function("return 'Hello World'")
+  }
+  this.prototype.set_value = function (value) {
+    this.value = val
+  }
+  this.value = new Object()
+}
+const instance = new A()
+const ctx = vm()
+ctx.instance = instance
+ctx.eval(`
+const instance2 = new instance.constructor(); // OK，将创建一个新的，受隔离的 instance。
+instance.method()(); // OK，将返回 Hello World。
+instance2.method()(); // OK，将返回 Hello World。
+instance.method().constructor('return window')(); // 逃逸失败
+instance.value.constructor.constructor('return window')(); // 逃逸失败
+instance2.value.constructor.constructor('return window')(); // 逃逸失败
+const a = {};
+A.call(a);
+a.value.constructor.constructor('return window')(); // 逃逸失败
+instance.set_value(1); // 成功
+instance2.set_value(1); // 成功
+`)
+```
+
+## 原理
+
+iframe 在被加入 DOM 树时，会生成一个 Window 对象放置于 iframe.contentWindow。
+
+当 iframe 被从 DOM 树删除时，iframe.contentWindow 将变为 null。
+
+而如果在删除之前先使用变量引用 contentWindow，再去删除 iframe，则会得到一个资源都被释放，但各种基础函数仍可用的 iframe。
+
+这将成为一个绝佳的沙盒环境。secure-vm 在其上配合 Proxy，实现了前端安全沙盒。
--- src/main.ts
+++ src/main.ts
@@ -1,27 +1,70 @@
+/**
+ * Copyright(c) 凌 2023.
+ * This project is licensed under the MIT license.
+ */
 type ScopeHelper<T extends readonly (string | symbol)[]> = {
   [key in T[number]]: unknown
 }
 const whitelist = [
+  'atob',
+  'btoa',
+  'clearInterval',
+  'clearTimeout',
+  'crypto',
+  'location',
+  'queueMicrotask',
+  'requestIdleCallback',
+  'setInterval',
+  'setTimeout',
+  'Infinity',
+  'AggregateError',
   'Array',
+  'ArrayBuffer',
+  'Atomics',
+  'BigInt',
+  'BigInt64Array',
+  'BitUint64Array',
   'Boolean',
+  'DataView',
   'Date',
   'Error',
   'EvalError',
+  'FinalizationRegistry',
+  'Float32Array',
+  'Float64Array',
   'Function',
-  'Infinity',
+  'Int8Array',
+  'Int16Array',
+  'Int32Array',
+  'Intl',
+  'Iterator',
   'JSON',
+  'Map',
   'Math',
   'NaN',
   'Number',
   'Object',
-  'Reflect',
+  'Promise',
+  'Proxy',
   'RangeError',
   'ReferenceError',
+  'Reflect',
   'RegExp',
+  'Set',
   'String',
+  'Symbol',
   'SyntaxError',
   'TypeError',
   'URIError',
+  'URL',
+  'Uint8Array',
+  'Uint8ClampedArray',
+  'Uint16Array',
+  'Uint32Array',
+  'WeakMap',
+  'WeakRef',
+  'WeakSet',
+  'WebAssembly',
   'decodeURI',
   'decodeURIComponent',
   'encodeURI',
@@ -33,7 +76,6 @@ const whitelist = [
   'parseFloat',
   'parseInt',
   'undefined',
-  'location',
   'unescape'
 ] as const
 type ContextScope = ScopeHelper<typeof whitelist> & {
@@ -44,98 +86,117 @@ type ContextScope = ScopeHelper<typeof whitelist> & {
   [key: string | symbol]: unknown
 }
 /**
- * 虚拟机上下文。
+ * 创建虚拟机上下文。
  */
-export class Context {
-  private context: ContextScope
-  /**
-   * 在上下文中执行代码。
-   * @param code 代码。
-   * @returns 代码的结果。
-   */
-  eval(code: string) {
-    return this.context.eval(code)
-  }
-  /**
-   * 创建上下文。
-   * @param ctx 在全局中的额外属性。将会使用 Proxy 确保安全。
-   */
-  constructor(ctx: { [key: string | symbol]: object } = {}) {
-    const proxify = (context: ContextScope, obj: object): object => {
-      const ensure_safe = (res: unknown): unknown => {
-        if (
-          res != null &&
-          (typeof res === 'object' || typeof res === 'function')
-        ) {
-          return proxify(context, res)
-        }
-        return res
+export function vm(): ContextScope {
+  const proxify = <T extends object>(context: ContextScope, obj: T): T => {
+    const ensure_safe = <T>(res: T): T => {
+      if (res == Function) return context.Function as T
+      if (
+        res != null &&
+        (typeof res === 'object' || typeof res === 'function')
+      ) {
+        return proxify(context, res)
       }
-      return new Proxy(obj, {
-        get(
-          target: object,
-          property: string | symbol,
-          receiver: unknown
-        ): unknown {
-          try {
-            const res = Reflect.get(target, property, receiver)
-            if (typeof target === 'function' && property === 'prototype') {
-              context.eval(`throw new TypeError(
+      return res
+    }
+    const safeify = <T extends Function>(fn: T): T => {
+      return function (this: unknown, ...args: unknown[]) {
+        try {
+          return ensure_safe(fn.apply(this, args))
+        } catch (e) {
+          return ensure_safe(e)
+        }
+      } as unknown as T
+    }
+    return new Proxy(obj, {
+      get(
+        target: object,
+        property: string | symbol,
+        receiver: unknown
+      ): unknown {
+        try {
+          const res = Reflect.get(target, property, receiver)
+          if (typeof target === 'function' && property === 'prototype') {
+            context.eval(`throw new TypeError(
               'Accessing prototype by constructor is not supported. Use Object.getPrototypeOf() or Reflect.getPrototypeOf() instead'
             )`)
-            }
-            if (res === Function) {
-              return context.Function
-            }
-            return ensure_safe(res)
-          } catch (e) {
-            throw ensure_safe(e)
           }
-        },
-        set(
-          target: object,
-          property: string | symbol,
-          newValue: unknown,
-          receiver: unknown
-        ): boolean {
-          try {
-            return Reflect.set(target, property, newValue, receiver)
-          } catch (e) {
-            throw ensure_safe(e)
+          if (res === Function) {
+            return context.Function
           }
-        },
-        apply(target: object, thisArg: unknown, argArray: unknown[]): unknown {
-          try {
+          return ensure_safe(res)
+        } catch (e) {
+          throw ensure_safe(e)
+        }
+      },
+      set(
+        target: object,
+        property: string | symbol,
+        newValue: unknown,
+        receiver: unknown
+      ): boolean {
+        try {
+          if (
+            newValue != null &&
+            (typeof newValue === 'function' || typeof newValue === 'object') &&
+            newValue.constructor.constructor === Function
+          ) {
+            // 外部方法正在向 context 写入内容：这可能会导致危险操作
+            return Reflect.set(
+              target,
+              property,
+              ensure_safe(newValue),
+              receiver
+            ) // 为避免在外部方法上伪造 this 致方法泄露
+          }
+          return Reflect.set(target, property, newValue, receiver)
+        } catch (e) {
+          throw ensure_safe(e)
+        }
+      },
+      apply(target: object, thisArg: unknown, argArray: unknown[]): unknown {
+        try {
+          if (thisArg == undefined) {
+            // 防止当 thisArg 为 undefined 时函数污染主页面作用域。
             return ensure_safe(
               Reflect.apply(
                 target as (this: unknown, ...args: unknown[]) => unknown,
-                thisArg,
+                proxify(context, context),
                 argArray
               )
             )
-          } catch (e) {
-            return ensure_safe(e)
           }
-        },
-        construct(
-          target: object,
-          argArray: unknown[],
-          newTarget: Function
-        ): object {
-          try {
-            return proxify(
-              context,
-              Reflect.construct(
-                target as new (...args: unknown[]) => object,
-                argArray,
-                newTarget
-              )
+          return ensure_safe(
+            Reflect.apply(
+              target as (this: unknown, ...args: unknown[]) => unknown,
+              thisArg,
+              argArray
             )
-          } catch (e) {
-            throw ensure_safe(e)
-          }
-        },
-        getPrototypeOf(target: object): object | null {
+          )
+        } catch (e) {
+          return ensure_safe(e)
+        }
+      },
+      construct(
+        target: object,
+        argArray: unknown[]
+        // newTarget: Function // prototype access violation
+      ): object {
+        try {
+          return proxify(
+            context,
+            Reflect.construct(
+              target as new (...args: unknown[]) => object,
+              argArray
+            )
+          )
+        } catch (e) {
+          throw ensure_safe(e)
+        }
+      },
+      getPrototypeOf(target: object): object | null {
+        try {
           const res = Reflect.getPrototypeOf(target)
           if (res === null) return res
           const temp = {}
@@ -143,56 +204,127 @@ export class Context {
             Reflect.set(temp, key, Reflect.get(res, key))
           )
           return proxify(context, temp)
-        },
-        isExtensible(): boolean {
-          return true
-        },
-        preventExtensions(): boolean {
-          return false
-        },
-        setPrototypeOf(): boolean {
-          return false
+        } catch (e) {
+          throw ensure_safe(e)
         }
-      })
-    }
-    const createContext = (
-      whitelist: readonly (string | symbol)[]
-    ): ContextScope => {
-      const elem = document.createElement('iframe')
-      elem.src = 'about:blank'
-      elem.style.display = 'none'
-      document.head.appendChild(elem)
-      const win = elem.contentWindow as unknown as ContextScope
-      document.head.removeChild(elem)
-      if (!win) throw new Error('Could not create context')
-      for (const key of Reflect.ownKeys(win)) {
-        if (!whitelist.includes(key)) {
+      },
+      defineProperty(
+        target: object,
+        property: string | symbol,
+        attributes: PropertyDescriptor
+      ): boolean {
+        try {
+          if (
+            attributes.value != undefined &&
+            (typeof attributes.value === 'function' ||
+              typeof attributes.value === 'object') &&
+            attributes.value.constructor.constructor === Function
+          ) {
+            const temp: Partial<PropertyDescriptor> = {}
+            temp.value = ensure_safe(attributes.value)
+            if (attributes.writable) temp.writable = attributes.writable
+            if (attributes.enumerable) temp.enumerable = attributes.enumerable
+            if (attributes.configurable)
+              temp.configurable = attributes.configurable
+            return Reflect.defineProperty(target, property, temp)
+          } else {
+            const { get, set } = attributes
+            if (
+              ((get != undefined && typeof get === 'function') ||
+                (set != undefined && typeof set === 'function')) &&
+              (get?.constructor === Function || set?.constructor === Function)
+            ) {
+              const temp: Partial<PropertyDescriptor> = {}
+              if (get) temp.get = safeify(get)
+              if (set) temp.set = safeify(set)
+              if (attributes.writable) temp.writable = attributes.writable
+              if (attributes.enumerable) temp.enumerable = attributes.enumerable
+              if (attributes.configurable)
+                temp.configurable = attributes.configurable
+              return Reflect.defineProperty(target, property, temp)
+            }
+          }
+          return Reflect.defineProperty(target, property, attributes)
+        } catch (e) {
+          throw ensure_safe(e)
+        }
+      },
+      getOwnPropertyDescriptor(
+        target: object,
+        property: string | symbol
+      ): PropertyDescriptor | undefined {
+        const res = Reflect.getOwnPropertyDescriptor(target, property)
+        if (res === undefined) return undefined
+        if (
+          res.value !== undefined &&
+          (typeof res.value === 'function' || typeof res.value === 'object') &&
+          res.value.constructor.constructor === Function
+        ) {
+          return {
+            writable: res.writable,
+            enumerable: res.enumerable,
+            configurable: res.configurable,
+            value: ensure_safe(res.value)
+          }
+        } else {
+          const { get, set } = res
+          if (
+            ((get != undefined && typeof get === 'function') ||
+              (set != undefined && typeof set === 'function')) &&
+            (get?.constructor === Function || set?.constructor === Function)
+          ) {
+            return {
+              enumerable: res.enumerable,
+              configurable: res.configurable,
+              get: ensure_safe(get),
+              set: ensure_safe(set)
+            }
+          }
+        }
+        return res
+      },
+      isExtensible(): boolean {
+        return true
+      },
+      preventExtensions(): boolean {
+        return false
+      },
+      setPrototypeOf(): boolean {
+        return false
+      }
+    }) as T
+  }
+  const createContext = (
+    whitelist: readonly (string | symbol)[]
+  ): ContextScope => {
+    const elem = document.createElement('iframe')
+    elem.src = 'about:blank'
+    elem.style.display = 'none'
+    document.head.appendChild(elem)
+    const win = elem.contentWindow as unknown as ContextScope
+    document.head.removeChild(elem)
+    if (!win) throw new Error('Could not create context')
+    for (const key of Reflect.ownKeys(win)) {
+      if (!whitelist.includes(key)) {
+        try {
+          if (Object.is(Reflect.get(win, key), win)) continue
+        } catch (_) {} // Firefox 118: Reflect.get(win, 'screen') -> NS_ERROR_UNEXPECTED internal error
+        if (!Reflect.deleteProperty(win, key)) {
           try {
-            if (Object.is(Reflect.get(win, key), win)) continue
-          } catch (_) {} // Firefox 118: Reflect.get(win, 'screen') -> NS_ERROR_UNEXPECTED internal error
-          if (!Reflect.deleteProperty(win, key)) {
-            try {
-              Reflect.set(win, key, undefined)
-              if (Reflect.get(win, key) != undefined) throw new Error()
-            } catch (_) {
-              const val = Reflect.get(win, key)
-              if (typeof val === 'object' && val !== null) {
-                if (!Reflect.setPrototypeOf(val, null)) {
-                  console.warn('Cannot delete property', key)
-                }
-                Reflect.preventExtensions(val)
-              } else {
-                console.warn('Cannot delete property', key)
-              }
+            Reflect.set(win, key, undefined)
+            if (Reflect.get(win, key) != undefined) throw new Error() // 即使是 null 也是可接受的
+          } catch (_) {
+            const val = Reflect.get(win, key)
+            if (typeof val === 'object' && val !== null) {
+              Reflect.setPrototypeOf(val, null)
+              Reflect.preventExtensions(val)
             }
           }
         }
       }
-      return win
     }
-    this.context = createContext(whitelist)
-    for (const [key, value] of Object.entries(ctx)) {
-      Reflect.set(this.context, key, proxify(this.context, value))
-    }
+    return win
   }
+  const context = createContext(whitelist)
+  return proxify(context, context)
 }
